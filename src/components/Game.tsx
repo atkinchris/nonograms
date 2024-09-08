a06a3db2a815b1a026d46c86dbdf13900f8067ec
Chris Atkin <atkinchris@gmail.com>
2024-09-08 21:04:58 +0100

    Colourise hints

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { FunctionComponent, useState } from 'react'
+import classNames from 'classnames'
 
 import { Cell, getColumnCells, getRowCells, toGroups } from '../utils/cells'
 import { iter } from '../utils/array'
@@ -22,15 +23,33 @@ const Game: FunctionComponent<Props> = ({ height, width, solution }) => {
   const [data, setData] = useState(solution)
   const [mode, setMode] = useState<Cell.Filled | Cell.Flagged>(Cell.Filled)
 
-  const rows = iter(height, y => ({
-    groups: rowRequirements[y],
-    isValid: toGroups(getRowCells(data, width, y)).join('') === rowRequirements[y].join(''),
-  }))
+  const rows = iter(height, y => {
+    const dataGroups = toGroups(getRowCells(data, width, y))
+    const groups = rowRequirements[y].map((group, i) => ({
+      value: group,
+      isValid: group === dataGroups[i],
+    }))
 
-  const columns = iter(width, x => ({
-    groups: colRequirements[x],
-    isValid: toGroups(getColumnCells(x, data, width)).join('') === colRequirements[x].join(''),
-  }))
+    return {
+      groups,
+      isValid: groups.every(group => group.isValid),
+      isDirty: dataGroups.length > 0,
+    }
+  })
+
+  const columns = iter(width, x => {
+    const dataGroups = toGroups(getColumnCells(x, data, width))
+    const groups = colRequirements[x].map((group, i) => ({
+      value: group,
+      isValid: group === dataGroups[i],
+    }))
+
+    return {
+      groups,
+      isValid: groups.every(group => group.isValid),
+      isDirty: dataGroups.length > 0,
+    }
+  })
 
   const handleCellClick = (x: number, y: number) => {
     const index = y * width + x
@@ -42,19 +61,23 @@ const Game: FunctionComponent<Props> = ({ height, width, solution }) => {
   return (
     <div className={styles.container}>
       <div className={styles.columnHints}>
-        {columns.map(({ groups }, index) => (
-          <div key={index}>
+        {columns.map(({ groups, isDirty, isValid }, index) => (
+          <div key={index} className={classNames(styles.hint, isDirty && !isValid && styles.error)}>
             {groups.map((group, i) => (
-              <span key={i}>{group}</span>
+              <span key={i} className={classNames(styles.hintValue, group.isValid && styles.satisfied)}>
+                {group.value}
+              </span>
             ))}
           </div>
         ))}
       </div>
       <div className={styles.rowHints}>
-        {rows.map(({ groups }, index) => (
-          <div key={index}>
+        {rows.map(({ groups, isDirty, isValid }, index) => (
+          <div key={index} className={classNames(styles.hint, isDirty && !isValid && styles.error)}>
             {groups.map((group, i) => (
-              <span key={i}>{group}</span>
+              <span key={i} className={classNames(styles.hintValue, group.isValid && styles.satisfied)}>
+                {group.value}
+              </span>
             ))}
           </div>
         ))}
